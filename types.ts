fc4006b85a5e2652f6bfb0fbfdc27fb0c9afabaf
Antti <anttituomola8@gmail.com>
2022-07-13 14:20:33 +0300

    Initial setup

---
+++ types.ts
@@ -0,0 +1,11 @@
+export interface Lautta {
+    id: string,
+    name: string,
+    location: string,
+    capacity: number,
+    pricemin: number
+    pricemax: number,
+    equipment: string[]
+    images: string[]
+    mainImage: string
+  }
