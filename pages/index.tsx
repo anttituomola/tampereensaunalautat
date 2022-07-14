837fc26226b7de21ead7380c18ae3934b53fcab0
Antti <anttituomola8@gmail.com>
2022-07-14 14:55:41 +0300

    Update prisma definition

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
-import { prisma } from '../lib/prisma'
+import prisma from '../lib/prisma'
 import { Lautta } from "../types"
 import LauttaEl from "../components/LauttaEl"
 import Filters from 'components/Filters'
