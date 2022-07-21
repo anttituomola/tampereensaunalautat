a1091b7154668c36463be2cb331c0873c5f37525
Antti <anttituomola8@gmail.com>
2022-07-21 09:36:49 +0300

    Floating action buttons on place

--- components/LauttaEl.tsx
+++ components/LauttaEl.tsx
@@ -1,6 +1,8 @@
 import { Lautta } from "../types"
 import Image from "next/image"
 import styles from "/styles/LauttaEl.module.css"
+import Fab from '@mui/material/Fab'
+import AddIcon from '@mui/icons-material/Add'
 
 type Props = {
     sauna: Lautta
@@ -8,15 +10,24 @@ type Props = {
 const LauttaEl = ({ sauna }: Props) => {
     console.log()
     return (
-        <div className={styles.lauttaEl}>
-            <div className={styles.imageHolder}>
-                <Image className={styles.theImage} src={`/images/${sauna.mainImage}`} alt={sauna.name} layout="fill" />
+        <>
+            <div className={styles.lauttaEl} >
+                <div className={styles.imageHolder}>
+                    <Image className={styles.theImage} src={`/images/${sauna.mainImage}`} alt={sauna.name} layout="fill" />
+                </div>
+                <h2>{sauna.name}</h2>
+                <p>{sauna.location}</p>
+                <p>Alkaen {sauna.pricemin} € / {sauna.eventLength} h</p>
+                <small>{sauna.notes}</small>
+                <div>
+                <Fab className={styles.lisaaPostitukseen} color="primary" size="small" aria-label="add">
+                    <AddIcon />
+                </Fab>
+
+                </div>
             </div>
-            <h2>{sauna.name}</h2>
-            <p>{sauna.location}</p>
-            <p>Alkaen {sauna.pricemin} € / {sauna.eventLength} h</p>
-            <small>{sauna.notes}</small>
-        </div>
+
+        </>
     )
 }
 export default LauttaEl
