87df8fd1a3a73f5572c68c7a0088927f0101b5f9
Antti <anttituomola8@gmail.com>
2022-07-22 14:40:31 +0300

    Client SES fix

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -1,19 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next"
-import aws from "aws-sdk"
+import { SESClient } from "@aws-sdk/client-ses"
+import { SendEmailCommand } from "@aws-sdk/client-ses"
 import dayjs from "dayjs"
 
-aws.config.update({
+/* aws.config.update({
   accessKeyId: process.env.ACCESS_KEY,
   secretAccessKey: process.env.SECRET_KEY,
   region: process.env.REGION,
-  signatureVersion: 'v4',
+}) */
+
+const credentials = {
+  accessKeyId: process.env.AWS_SDK_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SDK_SECRET_ACCESS_KEY,
+}
+
+const sesClient = new SESClient({
+  region: "us-east-1",
+  credentials: credentials,
 })
 
-var ses = new aws.SES({ region: "us-east-1" })
-
 type Data = {
-  response: string
+  message: string
 }
 
 type Req = {
@@ -35,28 +43,6 @@ export default function handler(
   const { emailTo, emailFrom, customerEmail, message }: Req = req.body
   console.log("req", req.body)
 
-  sesTest(emailTo, emailFrom, customerEmail, message)
-    .then((val) => {
-      console.log("Response from Amazon SES", val)
-      res.status(200).json({ response: "Successfully Sent Email" })
-    })
-    .catch((err) => {
-      res.status(400).json({ response: "error" })
-      console.log("There was an error!", err, req.body)
-    })
-}
-
-function sesTest(
-  emailTo: string,
-  emailFrom: string,
-  customerEmail: string,
-  message: {
-    date: string
-    time: string
-    pax: number
-    sauna: string
-  }
-) {
   var params = {
     Destination: {
       ToAddresses: [emailTo],
@@ -66,16 +52,33 @@ function sesTest(
         Text: {
           Data: `${message.sauna} on saanut tarjouspyynnön: 
           
-          Asiakas haluaisi saunoa ${dayjs(message.date).format("DD.MM.YYYY")} klo ${dayjs(message.time).format("HH:mm")}, mukana ${message.pax} osallistujaa.
+          Asiakas haluaisi saunoa ${dayjs(message.date).format(
+            "DD.MM.YYYY"
+          )} klo ${dayjs(message.time).format("HH:mm")}, mukana ${
+            message.pax
+          } osallistujaa.
           
           Vastaa asiakkaalle osoitteeseen ${customerEmail}, kertomalla sopiiko tämä aika saunasi kalenteriin ja mikä olisi tilaisuuden hinta.`,
         },
       },
-      Subject: { Data: "Tarjouspyyntö: " + dayjs(message.date).format("DD.MM.YYYY") },
+      Subject: {
+        Data: "Tarjouspyyntö: " + dayjs(message.date).format("DD.MM.YYYY"),
+      },
     },
-        ReplyToAddresses: [customerEmail],
-     Source: "info@tampereensaunalautat.fi",
+    ReplyToAddresses: [customerEmail],
+    Source: "info@tampereensaunalautat.fi",
   }
 
-  return ses.sendEmail(params).promise()
+  return sesClient
+    .send(new SendEmailCommand(params))
+    .then(() => {
+      res.status(200).json({
+        message: "Message sent.",
+      })
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: `Error occured: ${error}`,
+      })
+    })
 }
