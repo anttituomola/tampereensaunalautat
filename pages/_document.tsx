f63420856f9fc6055497624e272dfa54d8d0e48c
Antti <anttituomola8@gmail.com>
2022-07-25 12:01:13 +0300

    google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document {
     render() {
@@ -8,6 +9,19 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                     <link href="https://fonts.googleapis.com/css2?family=Albert+Sans:wght@100;300;600&display=swap" rel="stylesheet"></link>
+                    <Script
+                        src="https://www.googletagmanager.com/gtag/js?id=G-PBHP1Y4GEE"
+                        strategy="afterInteractive"
+                    />
+                    <Script id="google-analytics" strategy="afterInteractive">
+                        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-PBHP1Y4GEE');
+        `}
+                    </Script>
                 </Head>
                 <body>
                     <Main />
