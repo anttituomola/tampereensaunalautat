1244d674fa48d66106685aa8a0e4094c9095d13c
Antti <anttituomola8@gmail.com>
2022-07-27 20:52:06 +0300

    removed error button

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,4 @@
 import styles from "/styles/about.module.css"
-import Error from "components/Error"
 
 const about = () => {
   return (
@@ -8,7 +7,6 @@ const about = () => {
         <p>Ohjelmistokehittäjä <a href="https://www.anttituomola.fi" target="_blank" rel="noreferrer">Antti Tuomola</a> halusi järjestää ystävilleen illan saunalautalla, mutta huomasi, että Tampereen laajan saunalauttatarjonnan vertailu on varsin mutkikasta, sillä tietoa lautoista ei oltu koottu minnekään yhteen. Näin syntyi tampereensaunalautat.fi, ja internet - sekä Tampere - on taas piirun verran parempi paikka elää ja yrittää.</p>
         <h2>Oletko saunalauttayrittäjä?</h2>
         <p>Haluatko tietoihisi muutoksia? Eikö lauttaasi löydy tältä sivulta? Jotain muuta? Laita mailia: <a href="mailto:antti@anttituomola.fi">antti@anttituomola.fi</a></p>
-        <Error />
     </main>
     )
 }
